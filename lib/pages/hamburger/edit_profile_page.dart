e7603f865d6d7fa514bb2702fabf46aafdde1eee
SiriEiEi <sirimongkol.p@mail.kmutt.ac.th>
2022-03-27 10:46:38 +0700

    add image picker

--- lib/pages/hamburger/edit_profile_page.dart
+++ lib/pages/hamburger/edit_profile_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:xculturetestapi/model/user.dart';
 import 'package:xculturetestapi/utils/user_info.dart';
 import 'package:xculturetestapi/widgets/profile_widget.dart';
 import 'package:xculturetestapi/widgets/textfield_widget.dart';
+//import 'package:xculturetestapi/widgets/uploadprofile_widget.dart';
+import 'package:xculturetestapi/widgets/uploadprofilepic_widget.dart';
 
 class EditProfilePage extends StatefulWidget{
   @override
@@ -13,36 +16,55 @@ class _EditProfilePageState extends State<EditProfilePage>{
   User user = UserInfo.userTest;
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-      title: Text('Edit Profile'),
-      centerTitle: true,
-      backgroundColor: Colors.red,
-    ),
-    body: ListView(
-      padding: EdgeInsets.symmetric(horizontal: 32),
-      physics: BouncingScrollPhysics(),
-      children: [
-        const SizedBox(height: 20),
-        ProfileWidget(
-          imagePath: user.imagePath, 
-          isEdit : true,
-          onclicked: () async{},
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+        theme: ThemeData(
+        primarySwatch: Colors.red,
+        fontFamily: "Poppins",
+      ),
+        home: 
+          Scaffold(
+              appBar: AppBar(
+              title: Text('Edit Profile'),
+              centerTitle: true,
+              backgroundColor: Colors.red,
+            ),
+            body: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              physics: BouncingScrollPhysics(),
+              children: [
+                const SizedBox(height: 20),
+                //const UploadProfileImageWidget(),
+                 UploadProfilePicWidget(
+                  imagePath: user.imagePath, 
+                  isEdit : true,
+                  onclicked: () async{},
+                  ),
+                const SizedBox(height: 20),
+                TextFieldWidget(
+                  label: 'Username', 
+                  text: user.username, 
+                  onChanged: (name){}
+                ),
+                const SizedBox(height: 20),
+                TextFieldWidget(
+                  label: 'About me', 
+                  text: user.about, 
+                  maxLines:5,
+                  onChanged: (about){},
+                ),
+                const SizedBox(height:20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(350, 50),
+                      ),
+                      onPressed: () async {}, 
+                        child: const Text("Edit Profile")
+                      ),        
+              ],
+            ),
           ),
-        const SizedBox(height: 20),
-        TextFieldWidget(
-          label: 'Username', 
-          text: user.username, 
-          onChanged: (name){}
-        ),
-        const SizedBox(height: 20),
-        TextFieldWidget(
-          label: 'About me', 
-          text: user.about, 
-          maxLines:7,
-          onChanged: (about){}
-        ),    
-      ],
-    ),
   );
+}
 }
