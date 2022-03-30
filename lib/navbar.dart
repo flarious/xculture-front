638418cb3a2ff1d8503213c3df18cf5788eea679
FennecFoxy11 <yositamook@gmail.com>
2022-03-31 04:45:43 +0700

    Fix Navbar

--- lib/navbar.dart
+++ lib/navbar.dart
@@ -1,37 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:xculturetestapi/pages/menu/menu_page.dart';
-import 'package:xculturetestapi/pages/profile/profile_screen.dart';
 import 'package:xculturetestapi/pages/search/search_page.dart';
 import 'package:xculturetestapi/pages/event/event_page.dart';
 import 'package:xculturetestapi/pages/forum/forum_home.dart';
 import 'package:xculturetestapi/pages/community/commu_page.dart';
-import 'package:xculturetestapi/pages/hamburger/profile_page.dart';
 
-class Navbar {
-  static List pages = [
+
+class Navbar extends StatefulWidget {
+  const Navbar({ Key? key, required this.currentIndex }) : super(key: key);
+
+  final int currentIndex;
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+
+
+  late int _index;
+
+  List pages = [
     const EventPage(),
     const SearchPage(),
     const ForumPage(),
     const CommuPage(),
-    ProfilePage()
   ];
+  
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.currentIndex;
+  }
 
-  static Widget navbar(context, int pageIndex) {
-    int currentIndex = pageIndex; 
-
+  @override
+  Widget build(BuildContext context) {
+    
     return BottomNavigationBar(
       items: const [
           BottomNavigationBarItem(label: "Event", icon: Icon(Icons.room)),
           BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
           BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
           BottomNavigationBarItem(label: "Community", icon: Icon(Icons.groups)),
-          BottomNavigationBarItem(label: "Profile", icon: Icon(Icons.person)),
+          BottomNavigationBarItem(label: "Hamburger", icon: Icon(Icons.density_medium_rounded)),
       ],
+      currentIndex: _index,
       onTap: (index) {
-        currentIndex = index;
-        Navigator.push(context, MaterialPageRoute(builder: (context) => pages[index]));
+        if ( index == 4 ) {
+          Scaffold.of(context).openEndDrawer();
+        } else {
+          _index = index;
+          Navigator.push(context, MaterialPageRoute(builder: (context) => pages[_index]));
+        } 
       },
-      currentIndex: currentIndex,
+        
       type: BottomNavigationBarType.fixed,
       backgroundColor: Colors.white,
       selectedItemColor: Color.fromARGB(255, 255, 183, 0),
@@ -39,6 +60,7 @@ class Navbar {
       showSelectedLabels: false,
       showUnselectedLabels: false,
       elevation: 0,
-    );
+    ); 
   }
 }
+
