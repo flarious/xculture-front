07d1a96f6159b53354c246e508f8b4c428ed112a
FennecFoxy11 <yositamook@gmail.com>
2022-03-31 04:50:00 +0700

    Edit variable

--- lib/utils/user_info.dart
+++ lib/utils/user_info.dart
@@ -1,10 +1,10 @@
 import 'package:xculturetestapi/model/user.dart';
 
 class UserInfo{
-  static const userTest = User(
+  static const userTest = UserProfile(
     //imagePath: 'https://images.unsplash.com/photo-1537151672256-6caf2e9f8c95?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
     //imagePath: 'https://images.unsplash.com/photo-1626548307930-deac221f87d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=834&q=80',
-    imagePath: 'https://i.pinimg.com/564x/84/04/14/840414002203a9fd3d1b4a3136e833ea.jpg',
+    imagePath: 'https://steamuserimages-a.akamaihd.net/ugc/869615759254530873/E55E7391CA55A2131421E856A144897064D32F82/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
     username: 'Chat_Eiei',
     email: 'SE@gmail.com',
     about: '3rd-year student, Faculty of Science. Interested in culture and knowledge exchange. And open-minded about every culture.',
